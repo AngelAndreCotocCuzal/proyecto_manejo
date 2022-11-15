e06ddcc96d1461f5a4133d06c767579680b696cd
Angel André Cotoc Cuzal <78944937+AngelAndreCotocCuzal@users.noreply.github.com>
2022-11-14 18:11:37 -0600

    subiendo todo

--- src/main/java/panvdev/wordpfd_pdfword/App.java
+++ src/main/java/panvdev/wordpfd_pdfword/App.java
@@ -9,11 +9,14 @@ public class App
         String borrar = "C:/directorio/a.pdf";
         Metodos met = new Metodos();
         main txt_pdf = new main();
+
+
+
         
-        //met.crearArchivos(archivo);
-        //met.escribirArchivo(archivo, "hola estamos escribiendo");
+        met.crearArchivos(archivo);
+        met.escribirArchivo(archivo, "hola estamos escribiendo");
         //met.escribirArchivo(archivo, "linea");
-        met.escribirArchivo(archivo, "a");
+        //met.escribirArchivo(archivo, "a");
         met.leerArchivo(archivo);
         met.ArchivoCarpeta(carpeta);
         met.BuscarArchivo(archivo);
