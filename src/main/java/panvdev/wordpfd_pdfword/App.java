a5015fe55351a036c3e3dcdd8a0045e35795cc63
Allan Eduardo Perez Ajanel <allanperezajanel@gmail.com>
2022-11-14 18:19:35 -0600

    Se creo el metodo Bsucar Palabra en la clase Metodos y se agrego el metodo a la App

--- src/main/java/panvdev/wordpfd_pdfword/App.java
+++ src/main/java/panvdev/wordpfd_pdfword/App.java
@@ -1,8 +1,10 @@
 package panvdev.wordpfd_pdfword;
 
-public class App 
+import java.io.IOException;
+
+public class App
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String archivo = "C:/directorio/nuevo_prueba.txt";
         String carpeta = "C:/directorio/";
